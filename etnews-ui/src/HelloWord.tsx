d0efe6ea75f7f33109340006d7136db1a0111efe
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-10-25 23:21:43 -0300

    Atributo com JSX

--- etnews-ui/src/HelloWord.tsx
+++ etnews-ui/src/HelloWord.tsx
@@ -1,8 +1,16 @@
 export default function HelloWord() {
-
+    
     const name = 'Emanuel';
+    const title = <h1>Hello, {name} !!!</h1>
+    const index = 0;
 
+    if(name === 'Emanuel') {
     return <div>
-        <h1>Hello, { name === 'Emanuel' ? 'Emanuel Jr' : name } !!!</h1>
+      <h1 tabIndex={ index } style={{ background: 'red' }} > { name }</h1>
+    </div>
+    }
+    
+    return <div>
+       { title }
     </div>
 }
