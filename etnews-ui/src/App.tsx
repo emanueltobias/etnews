3a8b3bdbecbfaf8b70a66740382f6e44b15f9b5a
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-11-06 01:00:07 -0300

    Como o JSX é Renderizado no DOM

--- etnews-ui/src/App.tsx
+++ etnews-ui/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HelloWord from './HelloWord';
+
+const Component = React.createElement('div', { style: { background: 'red' } }, "Olá, mundo!");
+
+const HTMLElement = document.createElement('div')
+HTMLElement.style.background = 'red'
+HTMLElement.innerText = 'Olá, mundo!'
+
+console.log(Component);
+console.dir(HTMLElement);
+
+//@ts-ignore
+//document.body.appendChild(Component)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HelloWord />
+
+        { Component }
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Olá, mundo!
