734b7936083c1a606e850f32d2620b00df3e47e6
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-11-07 00:19:28 -0300

    Atualizando componente em tempo real

--- etnews-ui/src/App.tsx
+++ etnews-ui/src/App.tsx
@@ -1,38 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-const Component = React.createElement('div', { style: { background: 'red' } }, "Olá, mundo!");
-
-const HTMLElement = document.createElement('div')
-HTMLElement.style.background = 'red'
-HTMLElement.innerText = 'Olá, mundo!'
-
-console.log(Component);
-console.dir(HTMLElement);
-
-//@ts-ignore
-//document.body.appendChild(Component)
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        { Component }
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá, mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Clock />
       </header>
     </div>
   );
