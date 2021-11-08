4a1be033ee48fb39fd053e277a97315f2e96f630
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-11-08 00:22:29 -0300

    Estilizado componentes com Styled Components

--- etnews-ui/src/components/Card.tsx
+++ etnews-ui/src/components/Card.tsx
@@ -1,14 +1,15 @@
 import { PropsWithChildren } from "react";
 import './Card.css'
+import * as C from './Card.styles';
 
-type CardProps = PropsWithChildren<{ 
+type CardProps = PropsWithChildren<{
     title: string
     align?: 'center' | 'left' | 'right'
 }>
 export default function Card(props: CardProps) {
-    return <div className={`Card ${props.align ||  'center'}`}>
-        <div>{props.title}</div>
-       {props.children}
-    </div>
+    return <C.Wrapper align={props.align || 'center'}>
+        <C.Title>{props.title}</C.Title>
+        {props.children}
+    </C.Wrapper>
 
 }
