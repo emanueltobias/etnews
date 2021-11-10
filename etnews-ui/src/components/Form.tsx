25921516bada6d525d242ffe50741108811a21fe
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-11-10 00:27:33 -0300

    Dica preciosa para lidar com formulários

--- etnews-ui/src/components/Form.tsx
+++ etnews-ui/src/components/Form.tsx
@@ -1,16 +1,74 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 export default function Form() {
-    const [name, setName] = useState('Emanuel')
-    return <div>
-        <input
-            placeholder="Nome"
-            type="text"
-            value={name}
-            onChange={e => setName(e.target.value)}
-        />
+    const [form, setForm] = useState({
+        name: '',
+        lastName: '',
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    })
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        console.log(form)
+    }
+
+    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return <form onSubmit={handleSubmit}>
         <div>
-            {name}
+            <input
+                placeholder="Nome"
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleInputChange}
+            />
         </div>
-    </div>
+        <div>
+            <input
+                placeholder="Sobrenome"
+                type="text"
+                name="lastName"
+                value={form.lastName}
+                onChange={handleInputChange}
+            />
+        </div>
+        <div>
+            <input
+                placeholder="Email"
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleInputChange}
+            />
+        </div>
+        <div>
+            <input
+                placeholder="Senha"
+                name="password"
+                type="password"
+                value={form.password}
+                onChange={handleInputChange}
+            />
+        </div>
+        <div>
+            <input
+                placeholder="Confirme sua senha"
+                name="passwordConfirm"
+                type="password"
+                value={form.passwordConfirm}
+                onChange={handleInputChange}
+            />
+        </div>
+        <button type="submit">
+            enviar
+        </button>
+    </form>
 }
