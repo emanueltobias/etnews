1769afc69aef96a50164e277e4d83517ea6dfa5c
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-11-13 18:36:22 -0300

    "Ciclos de vida" com o hook useEffect

--- etnews-ui/src/App.tsx
+++ etnews-ui/src/App.tsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const names = ['Emanuel', 'Joyce', 'Samuel', 'Emanuel Jr', 'Arthur']
+
+  const [names, setNames] = useState([
+    'Emanuel', 'Joyce', 'Samuel', 'Emanuel Jr', 'Arthur'
+  ])
+
+  useEffect(() => {
+    console.log('Componente montado')
+
+    return() => {
+      console.log('Componente desmontado')
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-      <ul>
+        <ul>
           {
             names.map((name, index) => <li key={index}>{name}</li>)
           }
         </ul>
+        <button
+          onClick={() => {
+            setNames([...names, 'José'])
+          }}
+        >
+          Event
+        </button>
       </header>
     </div>
   );
--- etnews-ui/src/index.tsx
+++ etnews-ui/src/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+function Event() {
+  const [canViewApp, setCanViewApp] =
+    useState(true)
+  return <>
+    {
+      canViewApp &&
+      <App />
+    }
+    <button
+      onClick={() => {
+        setCanViewApp(!canViewApp)
+      }}
+    >
+      canViewApp
+    </button>
+  </>
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Event />
   </React.StrictMode>,
   document.getElementById('root')
 );
