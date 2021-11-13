541deed25be4d2385ddf0e8c6c9d0fac31782604
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2021-11-13 18:56:20 -0300

    Criado hook personalizado

--- etnews-ui/src/App.tsx
+++ etnews-ui/src/App.tsx
@@ -1,36 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import useCounter from './hooks/useCounter';
 
 function App() {
-
-  const [names, setNames] = useState([
-    'Emanuel', 'Joyce', 'Samuel', 'Emanuel Jr', 'Arthur'
-  ])
-
-  useEffect(() => {
-    console.log('Componente montado')
-
-    return() => {
-      console.log('Componente desmontado')
-    }
-  }, [])
-
+  const number = useCounter()
   return (
     <div className="App">
-      <header className="App-header">
-        <ul>
-          {
-            names.map((name, index) => <li key={index}>{name}</li>)
-          }
-        </ul>
-        <button
-          onClick={() => {
-            setNames([...names, 'José'])
-          }}
-        >
-          Event
-        </button>
-      </header>
+      { number }
     </div>
   );
 }
--- etnews-ui/src/index.tsx
+++ etnews-ui/src/index.tsx
@@ -1,30 +1,12 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-function Event() {
-  const [canViewApp, setCanViewApp] =
-    useState(true)
-  return <>
-    {
-      canViewApp &&
-      <App />
-    }
-    <button
-      onClick={() => {
-        setCanViewApp(!canViewApp)
-      }}
-    >
-      canViewApp
-    </button>
-  </>
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <Event />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
